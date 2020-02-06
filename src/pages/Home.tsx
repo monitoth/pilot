33d1ec78ed090f29dba0a10b32901684d5fc48b6
Monika Csorjan <mt@valgbar.com>
2020-02-06 20:02:31 +0200

    add classnames package, change version to v0.0.8

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, useIonViewDidEnter } from '@ionic/react';
 import React from 'react';
 import { isPlatform } from '@ionic/react';
+import classNames from 'classnames';
 import {desktopUpdater, closeNotification, restartApp} from '../js/desktopUpdater';
 import styles from './Home.module.css';
 
@@ -30,12 +31,12 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        Crossplatform application
+        Crossplatform Pilot application
         <p id="version"></p>    
-        <div id="notification" className={styles.hidden}>
+        <div id="notification" className={classNames(styles.notification, styles.hidden)}>
           <p id="message"></p>
           <IonButton id="close-button" color="primary" onClick={onCloseNotificationBox}>Close</IonButton>
-          <IonButton id="restart-button" color="secondary" onClick={onRestartDesktopApp} class={styles.hidden}>Restart</IonButton>
+          <IonButton id="restart-button" color="secondary" onClick={onRestartDesktopApp} className={styles.hidden}>Restart</IonButton>
         </div>        
       </IonContent>
     </IonPage>
